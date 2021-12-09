5b2f92af1a97e074e93a51f88eaf244b2fc05451
David.Guardia <david5050885@yahoo.es>
2021-12-09 14:58:29 +0100

    added disable restaurant route

--- routes/business.routes.js
+++ routes/business.routes.js
@@ -33,7 +33,7 @@ router.post("/:id/create", (req, res, next) => {
     });
 });
 
-//  GET /business/id/details -  Retrieves all of the projects
+//  GET /business/id/details -  Retrieves the details of a business
 router.get("/:id/details", (req, res, next) => {
   const {id} = req.params
   
@@ -62,50 +62,21 @@ router.get("/:id/details", (req, res, next) => {
     });
 });
 
-//  GET /api/projects/:projectId -  Retrieves a specific project by id
-router.get("/projects/:projectId", (req, res, next) => {
-  const { projectId } = req.params;
 
-  if (!mongoose.Types.ObjectId.isValid(projectId)) {
+// PUT  /:id/disable  -  Disables a specific business by id
+router.put("/:id/delete", (req, res, next) => {
+  const { id } = req.params;
+  console.log("------------",id)
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
     res.status(400).json({ message: "Specified id is not valid" });
     return;
   }
 
-  // Each Project document has `tasks` array holding `_id`s of Task documents
-  // We use .populate() method to get swap the `_id`s for the actual Task documents
-  Business.findById(projectId)
-    .populate("tasks")
-    .then((project) => res.status(200).json(project))
-    .catch((error) => res.json(error));
-});
-
-// PUT  /api/projects/:projectId  -  Updates a specific project by id
-router.put("/projects/:projectId", (req, res, next) => {
-  const { projectId } = req.params;
-
-  if (!mongoose.Types.ObjectId.isValid(projectId)) {
-    res.status(400).json({ message: "Specified id is not valid" });
-    return;
-  }
-
-  Project.findByIdAndUpdate(projectId, req.body, { new: true })
-    .then((updatedProject) => res.json(updatedProject))
-    .catch((error) => res.json(error));
-});
-
-// DELETE  /api/projects/:projectId  -  Deletes a specific project by id
-router.delete("/projects/:projectId", (req, res, next) => {
-  const { projectId } = req.params;
-
-  if (!mongoose.Types.ObjectId.isValid(projectId)) {
-    res.status(400).json({ message: "Specified id is not valid" });
-    return;
-  }
-
-  Project.findByIdAndRemove(projectId)
-    .then(() =>
+  Business.findByIdAndUpdate(id, {isProfileComplete: false})
+    .then((response) =>
       res.json({
-        message: `Project with ${projectId} is removed successfully.`,
+        message: `Business with ${id} is removed successfully.`,
       })
     )
     .catch((error) => res.json(error));
