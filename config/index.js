de21d62492378c1344f4e1224b2be23ec575c835
David Guardia <89933637+DavidGV10@users.noreply.github.com>
2023-02-09 19:19:06 +0100

    Update index.js

--- config/index.js
+++ config/index.js
@@ -19,7 +19,7 @@ module.exports = (app) => {
 
   app.use(
     cors({
-      origin: [process.env.FRONTEND_DOMAIN],
+      origin: [process.env.FRONTEND_DOMAIN, "https://mesa-app.netlify.app/"],
     })
   );
 
