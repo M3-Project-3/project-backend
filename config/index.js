e152f7cd52c8c37d6210987545a26c3c8f263b23
David Guardia <89933637+DavidGV10@users.noreply.github.com>
2023-02-11 09:32:34 +0100

    Update index.js

--- config/index.js
+++ config/index.js
@@ -17,11 +17,11 @@ module.exports = (app) => {
   // Services like heroku use something called a proxy and you need to add this to your server
   app.set("trust proxy", 1);
   
-  //app.use(function(req, res, next) {
-  //res.header("Access-Control-Allow-Origin", "*");
-  //res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  //next();
-  //});
+  app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+  });
   
   app.use(
     cors({
